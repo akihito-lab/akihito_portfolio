481371134d81a3ab1617d249d5f89d1625a486dc
akihito-lab <fukudaakihito5@gmail.com>
2021-09-10 01:04:56 +0900

    9/10

--- js/main.js
+++ js/main.js
@@ -2,5 +2,17 @@ $(function() {
     $('.nav_toggle').on('click', function () {
         $('.nav_toggle, .click-nav').toggleClass('show');
       });
-      
+      if (window.matchMedia('(min-width: 769px)').matches) {
+        /* ウィンドウサイズ668以下の処理を記述 */
+        var TopHeight = $('.top').outerHeight(true);
+        $(window).scroll(function () {
+          if ($(this).scrollTop() > TopHeight) {
+            $('.nav-list').addClass('open');
+            $('.nav_toggle').fadeIn(800);
+          } else {
+            $('.nav-list').removeClass('open');
+            $('.nav_toggle').fadeOut(600);
+          }
+      });
+      } 
 });
