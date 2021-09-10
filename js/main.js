8e8b96f8080cad36894a58f9b744e0e85a906165
akihito-lab <fukudaakihito5@gmail.com>
2021-09-11 01:28:55 +0900

    9/11

--- js/main.js
+++ js/main.js
@@ -1,18 +1,36 @@
 $(function() {
+
     $('.nav_toggle').on('click', function () {
         $('.nav_toggle, .click-nav').toggleClass('show');
       });
+
+
       if (window.matchMedia('(min-width: 769px)').matches) {
         /* ウィンドウサイズ668以下の処理を記述 */
-        var TopHeight = $('.top').outerHeight(true);
         $(window).scroll(function () {
+          var TopHeight = $('.top').outerHeight(true);
           if ($(this).scrollTop() > TopHeight) {
-            $('.nav-list').addClass('open');
+            $('.nav-list').addClass('Telescopic');
             $('.nav_toggle').fadeIn(800);
           } else {
-            $('.nav-list').removeClass('open');
+            $('.nav-list').removeClass('Telescopic');
             $('.nav_toggle').fadeOut(600);
           }
       });
-      } 
+      }
+
+
+      $(window).on('scroll', function (){
+      $('.works-list>.item>.text>.title').each(function () {
+        var changeOffset = $(this).offset().top;
+        var scrolltop = $(window).scrollTop();
+        var wh = $(window).height();
+        if(scrolltop > changeOffset - wh / 2 ){
+        $(this).addClass('change');
+        }
+      });
+    });
+
+
+    
 });
